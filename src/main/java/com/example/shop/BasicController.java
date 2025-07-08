c5d577dae6e73f2fa4ede8083ebc8fb57c9fc690
Jack <devjack050700@gmail.com>
2025-07-09 03:04:17 +0900

    part1 : 웹서버와 웹페이지 만들기 (Controller)

---
+++ src/main/java/com/example/shop/BasicController.java
@@ -0,0 +1,25 @@
+package com.example.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    String hello(){
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String about(){
+        return "이 사이트는 사기 사이트입니다";
+    }
+
+    @GetMapping("/mypage")
+    @ResponseBody
+    String mypage(){
+        return "내 사이트다";
+    }
+}
