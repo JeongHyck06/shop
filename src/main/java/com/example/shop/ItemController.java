274bc0e5ea2c847652899bfc92f6ab6b10685a12
Jack <devjack050700@gmail.com>
2025-07-16 23:23:36 +0900

    Part1 : DB 데이터 출력하려면

--- src/main/java/com/example/shop/ItemController.java
+++ src/main/java/com/example/shop/ItemController.java
@@ -1,13 +1,19 @@
 package com.example.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+    private final ItemRepository itemRepository;
+
     @GetMapping("/list")
     String list(Model model) {
+        var result = itemRepository.findAll();
+        System.out.println(result);
         model.addAttribute("name", "홍길동");
         return "list";
     }
