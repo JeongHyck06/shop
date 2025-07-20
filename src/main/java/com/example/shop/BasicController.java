1726fe3cdca40fdf73d22a78a42831bb451d18d5
Jack <devjack050700@gmail.com>
2025-07-20 23:49:34 +0900

    Part1 : 상품 추가기능 1 (Map 자료형)

--- src/main/java/com/example/shop/BasicController.java
+++ src/main/java/com/example/shop/BasicController.java
@@ -2,10 +2,20 @@ package com.example.shop;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class BasicController {
+    private final ItemRepository itemRepository;
+
+    public BasicController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
     @GetMapping("/")
     String hello(){
         return "redirect:/index.html";
@@ -22,4 +32,18 @@ public class BasicController {
     String mypage(){
         return "내 사이트다";
     }
+
+    @GetMapping("/write")
+    String write(){
+        return "write";
+    }
+    @PostMapping("/add")
+    String addPost(@RequestParam String title, Integer price){
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+        itemRepository.save(item);
+        return "redirect:/list";
+    }
+
 }
--- src/main/java/com/example/shop/Item.java
+++ src/main/java/com/example/shop/Item.java
@@ -1,16 +1,20 @@
 package com.example.shop;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @ToString
+@Getter
+@Setter
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer id;
 
-    public String title;
-    public Integer price;
+    private String title;
+    private Integer price;
 
 //    public String toString() {
 //    return this.title + this.price;
